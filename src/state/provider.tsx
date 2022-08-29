4e4170595972e9c83ecea94855e0b9d36a65c517
Gehu Pierre <webdev@pierrestudios.com>
2022-08-29 13:14:56 -0400

    Added firebase

--- src/state/provider.tsx
+++ src/state/provider.tsx
@@ -1,7 +1,28 @@
 import { ReactNode, useState } from "react";
+import { initializeApp } from "firebase/app";
 
 import { ModalStateProps } from "../types/context";
 import AppContext from "./context";
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_API_DOMAIN,
+  REACT_APP_DB_URL,
+  REACT_APP__PROJECT_ID,
+  REACT_APP_BUCKET,
+  REACT_APP_MESSAGE_SENDER_ID,
+  REACT_APP_ID,
+} = process.env;
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_API_DOMAIN,
+  databaseURL: REACT_APP_DB_URL,
+  projectId: REACT_APP__PROJECT_ID,
+  storageBucket: REACT_APP_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGE_SENDER_ID,
+  appId: REACT_APP_ID,
+};
+const firebaseApp = initializeApp(firebaseConfig);
 
 interface Props {
   children: ReactNode;
